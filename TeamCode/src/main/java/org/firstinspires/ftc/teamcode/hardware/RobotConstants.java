64f80cf374c4d0ff5cc874b90b2826e400dd4b58
Ramalh0z <arthu.ramalh0z@gmail.com>
2023-01-25 11:53:56 -0300

    centralizando todas as constantes do robô em um só arquivo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotConstants.java
@@ -1,10 +1,32 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-public final class RobotConstants {
-    public static final double kCorretorJoystickX = 1.01;
-    public static final double kMecanumWheelsAngle = Math.PI / 4;
+import com.acmerobotics.dashboard.config.Config;
 
-    // motor data
-    public static final int kCORE_HEX_TICKS = 288;
-    public static final int kHD_HEX_TICKS = 1120;
+@Config
+public final class RobotConstants {
+
+    // Controles
+    public static double DRIVER_CONTROLLER_X_AXIS_CORRECTION = 1.01;
+
+    // Drivetrain
+    public static double MAX_SPEED = 1.0;
+    public static final double MECANUM_WHEELS_ANGLE = Math.PI / 4;
+    public static final int CORE_HEX_TICKS = 288;
+    public static final int HD_HEX_TICKS = 1120;
+
+    // Braço
+    public static double ARMS_POWER_SCALE = 0.7;
+    public static double CENTRO_MAX_POWER = 0.6;
+    public static double CENTRO_MIN_POWER = -1.0;
+
+    // Servos
+    public static double GARRA_A_POSITION_CLOSED = 0.85;
+    public static double GARRA_A_POSITION_OPEN = 0.6;
+
+    public static double GARRA_B_POSITION_CLOSED = 0.5;
+    public static double GARRA_B_POSITION_OPEN = 0.15;
+
+    public static double HAND_CLOSED_POSITION = 0.78;
+    public static double HAND_MAX_POSITION = 0.4;
+
 }
