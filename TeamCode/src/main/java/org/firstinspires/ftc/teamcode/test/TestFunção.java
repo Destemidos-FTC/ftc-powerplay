ac1fe52cbfbcc56cf1230bc99705ff37b1afd08a
João Victor <123490774+JoaoRosenhaim@users.noreply.github.com>
2023-01-27 13:52:34 -0300

    Código do braço ta mais simples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestFunção.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestFunção.java
@@ -9,6 +9,17 @@ public final class TestFunção {
     public static void girarBraço(double angulo, DestemidosBot robot)
     {
         
+        angulo = (TesteBraçoAutonomo.target);
+        ConvertDegreesToTicks(angulo);
+        angulo = target_in_ticks;
+
+        motorBraçoACurrentPosition = robo.braçoA.getCurrentPosition();
+
+        double pid = controller.calculate(motorBraçoACurrentPosition, target_in_ticks);
+        double ff = Math.cos(Math.toRadians(target)) * f;
+
+        double power = pid + ff; 
+
         robot.motorBraçoA.setPower(0); //TesteBraçoAutonomo.power);
         robot.motorBraçoB.setPower(0); //TesteBraçoAutonomo.power);
 
