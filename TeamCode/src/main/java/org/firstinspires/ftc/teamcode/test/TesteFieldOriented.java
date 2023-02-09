34f1a703ef9c1743442329c4fc7d55936454e6c7
Ramalh0z <arthu.ramalh0z@gmail.com>
2023-02-09 11:59:25 -0300

    removendo teste inutilizado e criando um teste para o novo tipo de movimentação

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TesteFieldOriented.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TesteFieldOriented.java
@@ -2,25 +2,28 @@ package org.firstinspires.ftc.teamcode.test;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.hardware.DestemidosBot;
+import org.firstinspires.ftc.teamcode.subsystems.MovementSystem;
 
-@Autonomous(name = "TesteDrivetrainSystem", group = "Test")
-public class TesteDrivetrainSystem extends LinearOpMode {
+@TeleOp(name = "Teste - Field Oriented", group = "Test")
+public class TesteFieldOriented extends LinearOpMode{
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         // setup the hardware components
         DestemidosBot robot = new DestemidosBot(hardwareMap);
+        MovementSystem movementSystem = new MovementSystem(robot);
+
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
         while(opModeIsActive()) {
 
-            //
+            movementSystem.fieldOrientedController(gamepad1);
 
             telemetry.update();
         }
