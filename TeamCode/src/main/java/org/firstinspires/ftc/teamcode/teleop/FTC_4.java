794d198a67bc63c6e217fb6e23ef670f3dcc02f4
Arthur Ramalho <arthu.ramalh0z@gmail.com>
2023-02-27 11:02:42 -0300

    portando o FTC_4 pra o command based

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/FTC_4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/FTC_4.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -11,20 +13,34 @@ import org.firstinspires.ftc.teamcode.hardware.DestemidosBot;
  * e estabilidade por aqui
  */
 @TeleOp(name = "FTC_4", group = "TeleOps")
-public class FTC_4 extends LinearOpMode {
+public class FTC_4 extends CommandOpMode {
+    private DestemidosBot robot;
     @Override
-    public void runOpMode() {
-        final DestemidosBot robot = new DestemidosBot(hardwareMap);
+    public void initialize() {
+        // preparando o sistema de comandos
+        CommandScheduler.getInstance().reset();
 
-        waitForStart();
-        while (opModeIsActive()) {
-            robot.setBulkReadToAuto();
+        // configurçãoes do robô abaixo
+        robot = new DestemidosBot(hardwareMap);
+        robot.setBulkReadToAuto();
+    }
 
-            // movimentação padrão das partidas
-            robot.drivetrain.standardMecanumController(gamepad1);
+    @Override
+    public void run() {
+        double start_loop = System.currentTimeMillis();
 
-            // controles do braço e da mão
-            robot.armSystem.moveArms(gamepad2);
-        }
+        // controle da base
+        robot.drivetrain.standardMecanumController(gamepad1);
+
+        // controle do intake
+
+        // controle do outtake
+
+        // por fim, executa todos os sistemas
+        CommandScheduler.getInstance().run();
+
+        double end_loop = System.currentTimeMillis();
+        telemetry.addData("loop time", (end_loop - start_loop));
+        telemetry.update();
     }
 }
