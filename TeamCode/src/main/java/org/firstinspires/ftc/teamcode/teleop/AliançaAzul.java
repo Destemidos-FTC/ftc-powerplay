294cb316c4e09d61f58998ea60be8be8faf9c030
Ramalh0z <arthu.ramalh0z@gmail.com>
2023-03-10 06:41:36 -0300

    cleanup de subsistemas e teleops não utilizados

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AliançaAzul.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.ArmSystem;
+import org.firstinspires.ftc.teamcode.subsystems.DestemidosBot;
+
+@TeleOp
+public class AliançaAzul extends CommandOpMode {
+    private DestemidosBot robot;
+    private GamepadEx player2;
+
+    @Override
+    public void initialize() {
+        robot = new DestemidosBot(hardwareMap);
+        robot.setBulkReadToAuto();
+
+        player2 = new GamepadEx(gamepad2);
+        CommandScheduler.getInstance().reset();
+        register(robot.gripper, robot.armSystem, robot.forearmSystem);
+
+        player2.getGamepadButton(GamepadKeys.Button.DPAD_UP)
+                .whenActive(new InstantCommand(() -> robot.gripper.moveWrist(1)))
+                .whenInactive(new InstantCommand(() -> robot.gripper.moveWrist(0)));
+
+        player2.getGamepadButton(GamepadKeys.Button.DPAD_DOWN)
+                .whenActive(new InstantCommand(() -> robot.gripper.moveWrist(-1)))
+                .whenInactive(new InstantCommand(() -> robot.gripper.moveWrist(0)));
+
+        player2.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
+                .toggleWhenPressed(
+                        new InstantCommand(robot.gripper::closeGrip),
+                        new InstantCommand(robot.gripper::releaseGrip),
+                        true
+                );
+
+        player2.getGamepadButton(GamepadKeys.Button.A)
+                .whenPressed(
+                        new InstantCommand(() -> robot.armSystem.setArmPosition(ArmSystem.ArmStage.GROUND))
+                );
+
+        player2.getGamepadButton(GamepadKeys.Button.X)
+                .whenPressed(
+                        new InstantCommand(() -> robot.armSystem.setArmPosition(ArmSystem.ArmStage.GROUND))
+                );
+
+        player2.getGamepadButton(GamepadKeys.Button.Y)
+                .whenPressed(
+                        new InstantCommand(() -> robot.armSystem.setArmPosition(ArmSystem.ArmStage.LOW))
+                );
+
+        player2.getGamepadButton(GamepadKeys.Button.B)
+                .whenPressed(new InstantCommand(() -> robot.armSystem.setArmPosition(ArmSystem.ArmStage.MEDIUM))
+                );
+    }
+
+
+    @Override
+    public void run() {
+        double energy = robot.voltageSensor.getVoltage();
+
+        CommandScheduler.getInstance().run();
+
+        player2.readButtons();
+
+        robot.armSystem.setVoltage(energy);
+        robot.forearmSystem.moveForearmManually(gamepad2.right_stick_y);
+
+        robot.drivetrain.updateVoltage(energy);
+        robot.drivetrain.standardMecanumController(gamepad1);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TesteArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TesteArmSystem.java
@@ -6,23 +6,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.ArmSystem;
+import org.firstinspires.ftc.teamcode.subsystems.ForearmSystem;
 
 @TeleOp(name = "Teste ArmSystem", group = "Test")
 public class TesteArmSystem extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        ArmSystem armSystem = new ArmSystem(hardwareMap);
+        ArmSystem arms = new ArmSystem(hardwareMap);
+        ForearmSystem forearm = new ForearmSystem(hardwareMap);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
         while (opModeIsActive()) {
-            armSystem.moveArmsManually(gamepad2.left_stick_y);
-            armSystem.moveForearmManually(gamepad2.right_stick_y);
+            arms.moveArmsManually(gamepad2.left_stick_y);
+            forearm.moveForearmManually(gamepad2.right_stick_y);
 
-            telemetry.addData("armA - position", armSystem.armA.getCurrentPosition());
-            telemetry.addData("armB - position", armSystem.armB.getCurrentPosition());
-            telemetry.addData("armC - position", armSystem.armC.getCurrentPosition());
-            telemetry.addData("armD - position", armSystem.armD.getCurrentPosition());
+            telemetry.addData("armA - position", arms.armA.getCurrentPosition());
+            telemetry.addData("armB - position", arms.armB.getCurrentPosition());
+            telemetry.addData("armC - position", forearm.armC.getCurrentPosition());
+            telemetry.addData("armD - position", forearm.armD.getCurrentPosition());
             telemetry.update();
         }
     }
